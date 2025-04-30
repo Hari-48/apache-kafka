cef21c590fe934acf0f953a88434854a9acab737
Hari-0225 <harihara.sudhan@finsurge.tech>
2025-04-30 12:52:12 +0530

    kakfa Implementation

---
+++ cab-book-driver/src/main/java/com/hari/cab_book_driver/controller/CabLocationController.java
@@ -0,0 +1,40 @@
+package com.hari.cab_book_driver.controller;
+
+
+import com.hari.cab_book_driver.service.CabLocationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/location")
+public class CabLocationController {
+
+    @Autowired
+    CabLocationService cabLocationService;
+
+    @PutMapping
+    public ResponseEntity<?> updateLocation() throws InterruptedException {
+
+        int range = 10;
+        while (range > 0) {
+            //System.out.println(Math.random() + " , " + Math.random());
+            cabLocationService.updateLocation(Math.random() + " , " + Math.random());
+            Thread.sleep(10000);
+
+
+            range--;
+        }
+
+        return new ResponseEntity<>(Map.of("Message", "Location Update"), HttpStatus.OK);
+
+
+    }
+
+
+}
---
+++ cab-book-user/src/main/java/com/hari/cab_book_user/service/LocationService.java
@@ -0,0 +1,15 @@
+package com.hari.cab_book_user.service;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LocationService {
+
+
+    @KafkaListener(topics = "cab-location", groupId = "my-group")
+    public void getLocation(String location) {
+        System.out.println(location);
+
+    }
+}
